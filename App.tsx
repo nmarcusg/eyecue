3b213880762187caea089fb227228ab863d425e7
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2026-01-16 17:22:53 +0800

    feat: add text overlay for detected objects using worklets

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   useTensorflowModel,
 } from 'react-native-fast-tflite';
 import { useResizePlugin } from 'vision-camera-resize-plugin';
-
+import { Worklets } from 'react-native-worklets-core';
 import {
   Camera,
   useCameraDevice,
@@ -20,7 +20,20 @@ export default function App() {
   const { hasPermission, requestPermission } = useCameraPermission();
   const plugin = useTensorflowModel(require('./assets/yolov8n.tflite'));
   const { resize } = useResizePlugin();
-
+  const [label, setLabel] = useState<string>("Scanning...")
+  const updateLabel = Worklets.createRunOnJS((text: string) => {
+    setLabel(text);
+  })
+  const COCO_LABELS: string[] = [
+  'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+  'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+  'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+  'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle',
+  'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+  'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+  'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+  'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+];
   const frameProcessor = useFrameProcessor(
     (frame) => {
       'worklet';
@@ -55,6 +68,9 @@ export default function App() {
         }
 
         if (maxScore > confidence_threshold) {
+          const name = COCO_LABELS[classIndex];
+
+          updateLabel(name);
           console.log(
             `Detected Class #${classIndex} with ${Math.round(
               maxScore * 100
@@ -65,7 +81,7 @@ export default function App() {
 
       console.log(`First value: ${detectionData[0]}`);
     },
-    [plugin]
+    [plugin, updateLabel]
   );
 
   if (plugin.state === 'loading') {
@@ -112,6 +128,7 @@ export default function App() {
         isActive={toggleCamera}
         frameProcessor={frameProcessor}
       />
+      <Text>{label}</Text>
       <Button title="turn" onPress={() => setToggleCamera(!toggleCamera)} />
     </View>
   );
